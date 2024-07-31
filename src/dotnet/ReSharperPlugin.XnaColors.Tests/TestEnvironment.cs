0ef63538f611ccc80ed4834fa128413828596809
Stephen T <xxlennygamerxx@gmail.com>
2024-07-31 16:41:47 -0500

    Init project.

---
+++ src/dotnet/ReSharperPlugin.XnaColors.Tests/TestEnvironment.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.XnaColors.Tests;
+
+[ZoneDefinition]
+public class XnaColorsTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IXnaColorsZone>;
+
+[ZoneMarker]
+public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<XnaColorsTestEnvironmentZone>;
+
+[SetUpFixture]
+public class XnaColorsTestsAssembly : ExtensionTestEnvironmentAssembly<XnaColorsTestEnvironmentZone>;
---
+++ src/dotnet/ReSharperPlugin.XnaColors/IXnaColorsZone.cs
@@ -0,0 +1,7 @@
+using JetBrains.Application.BuildScript.Application.Zones;
+
+namespace ReSharperPlugin.XnaColors;
+
+[ZoneDefinition]
+// [ZoneDefinitionConfigurableFeature("Title", "Description", IsInProductSection: false)]
+public interface IXnaColorsZone : IZone;
